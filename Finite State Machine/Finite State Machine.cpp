b263282f37a2ac808087180ffe9c56b30fc19c8b
Antega1 <andreyka070106@gmail.com>
2023-12-29 16:38:07 +0300

    Update Finite State Machine.cpp

--- Finite State Machine/Finite State Machine.cpp
+++ Finite State Machine/Finite State Machine.cpp
@@ -1,62 +1,57 @@
 //Красиков Андрей 23.Б16
 
 #include <iostream>
-#include <stack>
-#include <string>
+#include <map>
+#include <set>
+#include <vector>
 
-enum class State {
-    VALID,
-    INVALID
-};
-
-class BracketAutomaton {
+template<typename T>
+class DFA {
 private:
-    std::stack<char> bracketStack;
-    State currentState;
+    std::set<int> states; // Множество состояний автомата
+    std::set<T> alphabet; // Алфавит автомата
+    std::map<std::pair<int, T>, int> transitionFunction; // Функция переходов
+    int initialState; // Начальное состояние
+    std::set<int> finalStates; // Множество конечных состояний
 
 public:
-    BracketAutomaton() : currentState(State::VALID) {}
+    DFA() : initialState(-1) { }
 
-    // Обработка одного символа
-    void processChar(char ch) {
-        switch (ch) {
-            // Если символ - открывающая скобка, добавляем её в стек
-            case '(': case '[': case '{':
-                bracketStack.push(ch);
-                break;
-            // Если символ - закрывающая скобка
-            default:
-                // Проверка на пустоту стека и соответствие скобок
-                if (bracketStack.empty() || 
-                    (ch == ')' && bracketStack.top() != '(') ||
-                    (ch == ']' && bracketStack.top() != '[') ||
-                    (ch == '}' && bracketStack.top() != '{')) {
-                    currentState = State::INVALID;
-                    return; // Прекращаем обработку при обнаружении ошибки
-                }
-                bracketStack.pop();
+    void addState(int state) {
+        states.insert(state);
+    }
+
+    void setInitialState(int state) {
+        if (states.find(state) != states.end()) {
+            initialState = state;
         }
     }
 
-    // Проверка на валидность скобочной последовательности
-    bool isValid() const {
-        return currentState == State::VALID && bracketStack.empty();
+    void addFinalState(int state) {
+        if (states.find(state) != states.end()) {
+            finalStates.insert(state);
+        }
+    }
+
+    void addTransition(int fromState, T symbol, int toState) {
+        if (states.find(fromState) != states.end() && states.find(toState) != states.end()) {
+            transitionFunction[{fromState, symbol}] = toState;
+            alphabet.insert(symbol);
+        }
+    }
+
+    bool isAccepted(const std::vector<T>& inputString) {
+        if (initialState == -1) return false; // Проверка наличия начального состояния
+
+        int currentState = initialState;
+        for (T symbol : inputString) {
+            auto it = transitionFunction.find({currentState, symbol});
+            if (it == transitionFunction.end()) {
+                return false; // Возвращаем false, если символ не найден в функции переходов
+            }
+            currentState = it->second;
+        }
+        return finalStates.find(currentState) != finalStates.end();
     }
 };
 
-int main() {
-    BracketAutomaton automaton;
-    std::string input;
-    std::cin >> input;
-
-    for (char ch : input) {
-        automaton.processChar(ch);
-        if (automaton.isValid() == false) {
-            break; // Прекращаем чтение, если последовательность невалидна
-        }
-    }
-
-    std::cout << (automaton.isValid() ? "Valid\n" : "Invalid\n");
-
-    return 0;
-}
